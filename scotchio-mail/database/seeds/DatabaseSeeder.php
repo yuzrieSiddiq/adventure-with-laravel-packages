c7777bb36e4a8a05ab367f11b01686e49a909eaf
Mohamad Yuzrie <yuyieyuzrie@gmail.com>
2017-01-19 10:53:05 +0800

    [INIT] Created controller and post route

---
+++ scotchio-mail/database/seeds/DatabaseSeeder.php
@@ -0,0 +1,21 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $this->call(UsersTableSeeder::class);
+        $this->call(RolesTableSeeder::class);
+        $this->call(PermissionsTableSeeder::class);
+        $this->call(UserPermissionsTableSeeder::class);
+        $this->call(UserRolesTableSeeder::class);
+        $this->call(RolesPermissionsTableSeeder::class);
+    }
+}
