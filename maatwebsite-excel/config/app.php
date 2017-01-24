d83daf526c481ec8dcdfac73ce212d397d476b42
Mohamad Yuzrie <yuyieyuzrie@gmail.com>
2017-01-24 14:27:41 +0800

    [INIT] setup service provider and facades

--- maatwebsite-excel/config/app.php
+++ maatwebsite-excel/config/app.php
@@ -168,6 +168,7 @@ return [
          */
         Spatie\Permission\PermissionServiceProvider::class,
         Spatie\Activitylog\ActivitylogServiceProvider::class,
+        Maatwebsite\Excel\ExcelServiceProvider::class,
         //
 
         /*
@@ -226,7 +227,7 @@ return [
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
-
+        'Excel' => Maatwebsite\Excel\Facades\Excel::class,
     ],
 
 ];
